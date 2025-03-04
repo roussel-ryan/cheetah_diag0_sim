2eb9919a6c600c3f657519952caf728a00ffc943
chris garnier <cgarnier@slac.stanford.edu>
2025-03-03 16:26:02 -0800

    committing broken version needed for debugging

--- beamdriver.py
+++ beamdriver.py
@@ -14,29 +14,32 @@ class SimDriver(Driver):
         self.screen = screen
 
     def read(self,reason):
+        print(f'{reason}')
         if 'Image:ArrayData' in reason and reason.rsplit(':',2)[0] == self.screen:
             madname = self.devices[self.screen]["madname"]
-            print(madname)
-            self.image_data = self.get_screen_distribution(screen_name = madname,
-            particle_beam=self.sim_beam,beamline=self.sim_beamline)
+            self.image_data = self.get_screen_distribution(screen_name = madname)
             value = list(self.image_data.flatten())
-        elif 'QUAD' and 'BCTRL' in reason:
+        elif 'QUAD' in reason and 'BCTRL' in reason:
+            print(f'reading {reason}')
             quad_name = reason.rsplit(':',1)[0]
             madname = self.devices[quad_name]["madname"]
-            value_from_segment = self.get_quad_value(madname, self.sim_beamline)
-            value = self.getParam(reason)
-            print(f"""segment value for {madname}  {value_from_segment},
-            matches pv value: {value}, {value_from_segment==value}""")
+            value = self.get_quad_value(madname)
+        elif 'QUAD' in reason and 'BACT' in reason:
+            print(f'reading {reason}')
+            quad_name = reason.rsplit(':',1)[0]
+            madname = self.devices[quad_name]["madname"]
+            value = self.get_quad_value(madname)
         else:
             value = self.getParam(reason)
         return value
         
     def write(self, reason, value):
-        if 'QUAD' and 'BCTRL' in reason:
+        if 'QUAD' in reason and 'BCTRL' in reason:
             quad_name = reason.rsplit(':',1)[0]
             madname = self.devices[quad_name]["madname"]
-            print(madname)
-            self.set_quad_value(madname,value,self.sim_beamline)
+            self.set_quad_value(madname,value)
+            self.setParam(reason,value)
+        elif 'QUAD' in reason and 'BACT' in reason:
             self.setParam(reason,value)
         elif 'QUAD' in reason:
             self.setParam(reason,value)
@@ -44,32 +47,37 @@ class SimDriver(Driver):
             print(f"""Write to OTRS pvs is disabled, 
                   failed to write to {reason}""")
     
-    def get_quad_value(self, quad_name:str, beamline: Segment):
-        names = [element.name for element in beamline.elements]
+
+    
+    def get_quad_value(self, quad_name:str):
+        names = [element.name for element in self.sim_beamline.elements]
+        print(quad_name)
+        print(names)
         if quad_name in names:
+            print('getting quad_value')
             index_num = names.index(quad_name)
-            quad_value = beamline.elements[index_num].k1.item()
+            quad_value = self.sim_beamline.elements[index_num].k1.item()
         else:
             print(f"""Warning {quad_name} not in Segment""")
             quad_value = np.nan
         return quad_value
 
-    def set_quad_value(self, quad_name:str, quad_value:float, beamline:Segment):
+    def set_quad_value(self, quad_name:str, quad_value:float):
         ''' Takes quad ctrl name and the k1 strength if the quad is in beamline'''
-        names = [element.name for element in beamline.elements]
+        names = [element.name for element in self.sim_beamline.elements]
         if quad_name in names:
             index_num = names.index(quad_name)
-            beamline.elements[index_num].k1 = torch.tensor(quad_value)
+            self.sim_beamline.elements[index_num].k1 = torch.tensor(quad_value)
             print(f"""Quad in segment with name {quad_name}
                    set to {quad_value}""")
 
-    def get_screen_distribution(self, screen_name:str, particle_beam: ParticleBeam, beamline:Segment )->list[float]:
+    def get_screen_distribution(self, screen_name:str)->list[float]:
         #TODO: put some safety stuff in here
-        beamline.track(particle_beam)
-        names = [element.name for element in beamline.elements]
+        self.sim_beamline.track(self.sim_beam)
+        names = [element.name for element in self.sim_beamline.elements]
         if screen_name in names:
             index_num = names.index(screen_name)
-            img = beamline.elements[index_num].reading
+            img = self.sim_beamline.elements[index_num].reading
             return img
         
 #TODO: start server takebeam size measurements and plot them in experiment.nb - image isn't processing
