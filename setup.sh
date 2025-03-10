74ba639c7fdee2cb4911ade9ae18e065458e1594
chris garnier <cgarnier@slac.stanford.edu>
2025-03-10 14:38:17 -0700

    some channel access issues were making the server unreliable, added another export statement, and instruction to launch caRepeater before starting server

--- setup.sh
+++ setup.sh
@@ -2,3 +2,5 @@
 export EPICS_CA_ADDR_LIST=127.0.0.1
 export EPICS_CA_AUTO_ADDR_LIST="NO"
 export EPICS_CA_MAX_ARRAY_BYTES=1000000000
+export EPICS_CA_SERVER_PORT=5064
+# run caReaper& in the terminal
