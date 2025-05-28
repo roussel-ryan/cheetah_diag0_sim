032895472feadbe9425763c7341da168861f5f59
chris garnier <cgarnier@slac.stanford.edu>
2025-05-28 15:10:49 -0700

    tagged a TODO for a bug fix here

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # If the particle distribution has not been extracted, extract it now
-[ ! -f impact_inj_output_YAG03.h5 ] && xz -k -d impact_inj_output_YAG03.h5.xz
+[ ! -f h5/impact_inj_output_YAG03.h5 ] && xz -k -d h5/impact_inj_output_YAG03.h5.xz
 
 export EPICS_CA_ADDR_LIST=127.0.0.1
 export EPICS_CA_AUTO_ADDR_LIST="NO"
--- simulated_server_diag0.py
+++ simulated_server_diag0.py
@@ -23,6 +23,7 @@ incoming_beam = ParticleBeam.from_twiss(
 #print(diag0_lattice)
 devices = load_relevant_controls('yaml/DIAG0.yaml')
 screen_name = 'OTRS:DIAG0:420'
+#TODO: fix some type of bug were defaults are not getting set from passable dictionary.... 
 screen_defaults = {'n_row': 1944, 'n_col': 1472, 'resolution': 23.33, 'pneumatic': 'OUT' }
 tcav_defaults = {}
 PVDB = create_pvdb(devices, **screen_defaults)
