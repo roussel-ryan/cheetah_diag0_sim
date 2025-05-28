9923419192b259e4e383d770de6152b041f5436c
chris garnier <cgarnier@slac.stanford.edu>
2025-05-28 15:29:11 -0700

    changed some stuff so notebooks/experiment.ipynb should still run

--- simulated_server.py
+++ simulated_server.py
@@ -5,20 +5,22 @@ from cheetah.accelerator import Segment
 import torch
 from utils.load_yaml import load_relevant_controls
 from utils.pvdb import create_pvdb
-
+import pprint 
 #design_incoming = ParticleBeam.from_openpmd_file(path='impact_inj_output_YAG03.h5', energy = torch.tensor(125e6),dtype=torch.float32)
 #lcls_lattice = Segment.from_lattice_json("lcls_cu_segment_otr2.json")
 design_incoming_beam = {'path': 'h5/impact_inj_output_YAG03.h5',
                          'energy': torch.tensor(125e6),
                          'dtype':torch.float32}
-lcls_lattice = 'lcls_cu_segment_otr2.json'
+lcls_lattice = 'lattices/lcls_cu_segment_otr2.json'
 devices = load_relevant_controls('yaml_configs/DL1.yaml')
 screen_name = 'OTRS:IN20:571'
-PVDB = create_pvdb(devices)
+screen_defaults = {'n_row': 1392, 'n_col': 1040, 'resolution': 4.65, 'pneumatic': 'OUT' }
+PVDB = create_pvdb(devices, **screen_defaults)
 custom_pvs = {'VIRT:BEAM:EMITTANCES': {'type':'float', 'count': 2},
               'VIRT:BEAM:RESET_SIM': {'value': 0}   
 }
 PVDB.update(custom_pvs)
+pprint.pprint(PVDB)
 server = SimpleServer()
 server.createPV('', PVDB)
 driver = SimDriver(screen=screen_name,
