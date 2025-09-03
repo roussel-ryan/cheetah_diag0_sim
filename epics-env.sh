8db27587eceb1a4f579d8b8ddf0b2bb3a189da85
Gopika Bhardwaj <bhardwaj.gopika@gmail.com>
2025-09-03 06:51:20 -0700

    Update epics-env.sh
    Fixing CA issue when running cheetah-diag0-sim

--- epics-env.sh
+++ epics-env.sh
@@ -3,7 +3,7 @@
 # Configures EPICS CA and PVA for use with Linac-Simulation-Server
 
 # Channel access settings
-export EPICS_CA_SERVER_PORT=10512
+#export EPICS_CA_SERVER_PORT=10512
 export EPICS_CA_ADDR_LIST=127.0.0.1
 export EPICS_CA_AUTO_ADDR_LIST=NO
 # Ensure we have enough space for large arrays
@@ -12,5 +12,5 @@ export EPICS_CA_MAX_ARRAY_BYTES=80000000
 # PVA settings
 export EPICS_PVA_SERVER_PORT=10513
 export EPICS_PVA_ADDR_LIST=127.0.0.1
-export EPCIS_PVA_AUTO_ADDR_LIST=YES
+export EPICS_PVA_AUTO_ADDR_LIST=YES
 
